b6315a5cf283064db4a03fdbfc7f4da3bef4bd90
Mateusz Chudek <m.chudek@kaft.pl>
2023-04-20 20:18:17 +0200

    Backend constant

--- constants/Backend.ts
+++ constants/Backend.ts
@@ -7,12 +7,10 @@ export const CONSTANTS_BACKEND_LOCALHOST = 'http://10.0.2.2:5079'
 export const CONSTANTS_BACKEND_CLOUD = 'https://xxx.azurewebsites.net';
 export const CONSTANTS_BACKEND_DIONIZOS = 'https://dionizos-backend-app.azurewebsites.net/';
 
-// export const BACKEND_URL =
-//   REACT_NATIVE_API ??
-//   Constants.expoConfig?.extra?.api ??
-// 	CONSTANTS_BACKEND_LOCALHOST;
-
-export const BACKEND_URL = CONSTANTS_BACKEND_DIONIZOS;
+export const BACKEND_URL =
+  REACT_NATIVE_API ??
+  Constants.expoConfig?.extra?.api ??
+	CONSTANTS_BACKEND_LOCALHOST;
 
 const Backend = (path: string) => BACKEND_URL + path;
 
